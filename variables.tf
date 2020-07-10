f4d83614273c496580c8b9c07d35c5ee219d9a0f
Warren Brown <warren@katapultmedia.com>
2020-07-10 09:22:33 -0500

    chore: updated terraform files for module

--- variables.tf
+++ variables.tf
@@ -1,31 +1,56 @@
-variable "application_name" {
-  description = "The name of the application"
+// TFC Config
+
+variable "auto_apply" {
+  default     = false
+  description = "Whether to automatically apply changes when a Terraform plan is successful. Defaults to false."
 }
 
-variable "aws_access_key_id" {
-  description = "AWS access key for the created workspaces"
-}
-
-variable "aws_default_region" {
-  description = "AWS default region for the created workspaces"
-}
-
-variable "aws_secret_access_key" {
-  description = "AWS secret key for the created workspaces"
+variable "name" {
+  description = "Name of the workspace."
 }
 
 variable "organization" {
-  description = "The name of the organization the application belongs to."
+  description = "The name of the organization the workspace is under."
 }
 
 variable "oauth_token_id" {
   description = "The OAuth token id for the VCS provider."
 }
 
-variable "tfe_token" {
-  default = ""
+variable "terraform_version" {
+  default     = "latest"
+  description = "The version of Terraform to use for this workspace. Defaults to the latest available version."
 }
 
-variable "vcs_repo" {
-  description = "The VCS repo where the Terraform exists."
+// VCS
+
+variable "repo" {
+  description = "A reference to your VCS repository in the format :org/:repo where :org and :repo refer to the organization and repository in your VCS provider."
+}
+
+variable "branch" {
+  description = "The repository branch that Terraform will execute from. Default to master."
+}
+
+variable "working_directory" {
+  description = "A relative path that Terraform will execute within. Defaults to the root of your repository."
+}
+
+// App Vars
+
+variable "secrets" {
+  default     = {}
+  description = "A map of custom secrets."
+  type        = map
+}
+
+variable "variables" {
+  default     = {}
+  description = "A map of custom variables."
+  type        = map
+}
+
+variable "hcl_vars" {
+  type    = map
+  default = {}
 }
