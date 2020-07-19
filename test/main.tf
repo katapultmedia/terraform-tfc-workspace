cded1530fb18ef915f56f412c75ba94e2639c247
Warren Brown <warren@katapultmedia.com>
2020-07-19 12:20:21 -0500

    chore: updated test/main.tf

--- test/main.tf
+++ test/main.tf
@@ -4,4 +4,12 @@ provider "aws" {
 
 module "core" {
   source = "../"
+
+  organization       = "organization-test"
+  branch             = "githubbranch-test"
+  github_tfe_ssh_key = "test-ssh-key"
+  working_directory  = "tfe-workspace-working-dir"
+  name               = "tfe-test-workspace"
+  oauth_token_id     = "alkasiopejk"
+  repo               = "tst-repo"
 }
